0ba379fc76f67d91d9f6acf1a2633e242fc02ff2
DaniiarR <danraiymkulov@mail.com>
2019-10-06 16:27:05 +0600

    Renamed the nav tabs

--- app/src/main/java/kg/gruzovoz/MainActivity.java
+++ app/src/main/java/kg/gruzovoz/MainActivity.java
@@ -20,13 +20,10 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText(R.string.title_orders);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText(R.string.title_history);
                     return true;
             }
             return false;
